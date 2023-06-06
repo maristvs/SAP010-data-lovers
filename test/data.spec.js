d63fe572769bd80876bdb047224f6896aaa43491
mairabergamaschi <mairabergama@gmail.com>
2023-06-05 22:24:36 -0300

    testes das funcoes searchName e sortCharacters

--- test/data.spec.js
+++ test/data.spec.js
@@ -66,3 +66,62 @@ describe('calculateFamilyPercentage', () => {
   });
 
 });
+
+describe('searchName', () => {
+  it('searchName is a function', () => {
+    expect(typeof dataHandler.searchName === 'function').toEqual(true);
+  });
+  const charactersArray = [
+    { fullName: 'Samwell Tarly', lastName: 'Tarly', firstName: 'Samwell' },
+    { fullName: 'Jon Snow', lastName: 'Snow', firstName: 'Jon' },
+    { fullName: 'Arya Stark', lastName: 'Stark', firstName: 'Arya' },
+  ];
+
+  it('When no matching names are found it should return an empty array', () => {
+    const result = dataHandler.searchName(charactersArray, 'Michael');
+    expect(result).toEqual([]);
+  });
+
+  it('When matching names are found it should return an array with matching names (case-insensitive)', () => {
+    const result1 = dataHandler.searchName(charactersArray, 'samwell');
+    expect(result1).toEqual([
+      { fullName: 'Samwell Tarly', lastName: 'Tarly', firstName: 'Samwell' },
+    ]);
+
+    const result2 = dataHandler.searchName(charactersArray, 'JON');
+    expect(result2).toEqual([
+      { fullName: 'Jon Snow', lastName: 'Snow', firstName: 'Jon' },
+    ]);
+
+    const result3 = dataHandler.searchName(charactersArray, 'arya');
+    expect(result3).toEqual([
+      { fullName: 'Arya Stark', lastName: 'Stark', firstName: 'Arya' },
+    ]);
+  }); 
+
+  describe('sortCharacters', () => {
+    it('sortCharacters is a function', () => {
+      expect(typeof dataHandler.sortCharacters === 'function').toEqual(true);
+    });
+  
+    it('When sorting in ascending order, the characters should be sorted alphabetically by full name', () => {
+      const sortedChars = dataHandler.sortCharacters(characters.got, 'ASC');
+      for (let i = 0; i < sortedChars.length - 1; i++) {
+        const currentChar = sortedChars[i];
+        const nextChar = sortedChars[i + 1];
+        const comparisonResult = currentChar.fullName.localeCompare(nextChar.fullName);
+        expect(comparisonResult).toBeLessThanOrEqual(0);
+      }
+    });
+  
+    it('When sorting in descending order, the characters should be sorted reverse alphabetically by full name', () => {
+      const sortedChars = dataHandler.sortCharacters(characters.got, 'DESC');
+      for (let i = 0; i < sortedChars.length - 1; i++) {
+        const currentChar = sortedChars[i];
+        const nextChar = sortedChars[i + 1];
+        const comparisonResult = currentChar.fullName.localeCompare(nextChar.fullName);
+        expect(comparisonResult).toBeGreaterThanOrEqual(0);
+      }
+    });
+  });
+}); 
