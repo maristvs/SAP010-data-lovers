f9404426a0381dfdf62fba3f5961eda291914085
mairabergamaschi <mairabergama@gmail.com>
2023-06-07 00:23:44 -0300

    criacao do arquivo mock.js e ajuste de testes

---
+++ test/charactersgotmock.js
@@ -0,0 +1,188 @@
+const charactersMock = [
+  {
+    "id": 0,
+    "firstName": "Daenerys",
+    "lastName": "Targaryen",
+    "fullName": "Daenerys Targaryen",
+    "title": "Mother of Dragons",
+    "family": "House Targaryen",
+    "imageUrl": "https://thronesapi.com/assets/images/daenerys.jpg",
+    "born": "284 DC"
+  },
+  {
+    "id": 1,
+    "firstName": "Samwell",
+    "lastName": "Tarly",
+    "fullName": "Samwell Tarly",
+    "title": "Maester",
+    "family": "House Tarly",
+    "imageUrl": "https://thronesapi.com/assets/images/sam.jpg",
+    "born": "283 DC"
+  },
+  {
+    "id": 2,
+    "firstName": "Jon",
+    "lastName": "Snow",
+    "fullName": "Jon Snow",
+    "title": "King of the North",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/jon-snow.jpg",
+    "born": "283 DC"
+  },
+  {
+    "id": 3,
+    "firstName": "Arya",
+    "lastName": "Stark",
+    "fullName": "Arya Stark",
+    "title": "No One",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/arya-stark.jpg",
+    "born": "289 AL"
+  },
+  {
+    "id": 4,
+    "firstName": "Sansa",
+    "lastName": "Stark",
+    "fullName": "Sansa Stark",
+    "title": "Lady of Winterfell",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/sansa-stark.jpeg",
+    "born": "286 AL"
+  },
+  {
+    "id": 5,
+    "firstName": "Brandon",
+    "lastName": "Stark",
+    "fullName": "Brandon Stark",
+    "title": "Lord of Winterfell",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/bran-stark.jpg",
+    "born": "290 DC"
+  },
+  {
+    "id": 6,
+    "firstName": "Ned",
+    "lastName": "Stark",
+    "fullName": "Ned Stark",
+    "title": "Lord of Winterfell",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/ned-stark.jpg",
+    "born": "263 DC"
+  },
+  {
+    "id": 7,
+    "firstName": "Cateyln",
+    "lastName": "Stark",
+    "fullName": "Catelyn Stark",
+    "title": "Lady of Winterfell",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/catelyn-stark.jpg",
+    "born": "264 DC",
+    "death": "299 DC"
+  },
+  {
+    "id": 8,
+    "firstName": "Robb",
+    "lastName": "Stark",
+    "fullName": "Rob Stark",
+    "title": "Lord of Winterfell",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/robb-stark.jpg",
+    "born": "283 DC",
+    "death": "299 DC"
+  },
+  {
+    "id": 9,
+    "firstName": "Ygritte",
+    "lastName": "None",
+    "fullName": "Ygritte",
+    "title": "Spearwife",
+    "family": "Free Folk",
+    "imageUrl": "https://thronesapi.com/assets/images/ygritte.jpg",
+    "born": "280 DC",
+    "death": "299 DC"
+  },
+  {
+    "id": 10,
+    "firstName": "Rickon",
+    "lastName": "Stark",
+    "fullName": "Rickon Stark",
+    "title": "Prince",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/rickon.jpg",
+    "born": "295 DC"
+  },
+  {
+    "id": 11,
+    "firstName": "Daario",
+    "lastName": "Naharis",
+    "fullName": "Daario",
+    "title": "Commander of the Second Sons",
+    "family": "Naharis",
+    "imageUrl": "https://thronesapi.com/assets/images/daario.jpg",
+    "born": null
+  },
+  {
+    "id": 12,
+    "firstName": "Shae",
+    "lastName": "",
+    "fullName": "Shae",
+    "title": "Mistress",
+    "family": "Lorathi",
+    "imageUrl": "https://thronesapi.com/assets/images/shae.jpg",
+    "born": "281 DC",
+    "death": "300 DC"
+  },
+  {
+    "id": 13,
+    "firstName": "Tommen",
+    "lastName": "Baratheon",
+    "fullName": "Tommen Baratheon",
+    "title": "Prince",
+    "family": "House Baratheon",
+    "imageUrl": "https://thronesapi.com/assets/images/tommen.jpg",
+    "born": "291 DC"
+  },
+  {
+    "id": 14,
+    "firstName": "Gendry",
+    "lastName": "Baratheon",
+    "fullName": "Gendry Baratheon",
+    "title": "Lord of Storm's End",
+    "family": "House Baratheon",
+    "imageUrl": "https://thronesapi.com/assets/images/gendry.jpg",
+    "born": "284 DC"
+  },
+  {
+    "id": 15,
+    "firstName": "Talisa",
+    "lastName": "Stark",
+    "fullName": "Talisa Stark",
+    "title": "Queen Consort",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/talisa-stark.jpg",
+    "born": null,
+    "death": "300 DC"
+  },
+  {
+    "id": 16,
+    "firstName": "Wylis",
+    "lastName": "Hodor",
+    "fullName": "Wylis Hodor",
+    "title": "Servant of House Stark",
+    "family": "House Stark",
+    "imageUrl": "https://thronesapi.com/assets/images/hodor.jpg",
+    "born": null
+  },
+  {
+    "id": 17,
+    "firstName": "Lord",
+    "lastName": "Bronn",
+    "fullName": "Lord Bronn",
+    "title": "Lord of Highgarden",
+    "family": "Bronn",
+    "imageUrl": "https://thronesapi.com/assets/images/bronn.jpg",
+    "born": "268 DC"
+  }
+]
+export default charactersMock;
